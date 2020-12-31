d471e170202246d85d5fdfaf71b8ca18e4594eb4
Lupita Rivera <lupitarivera8899@gmail.com>
2020-12-31 15:45:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ var kittens = [ 'Milo', 'Otis', 'Garfield'] //define your array here
 function destructivelyAppendKitten(name) {kittens.push(name);
 return kittens;
 }
+function Arrays destructivelyPrependKitten(name){kittens.unshift(name);
+return kittens;}
