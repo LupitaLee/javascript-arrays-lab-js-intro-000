5b229dc278fe992cfaae9812c0216a4ad68bd5ea
Lupita Rivera <lupitarivera8899@gmail.com>
2020-12-31 15:46:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var kittens = [ 'Milo', 'Otis', 'Garfield'] //define your array here
 // Add your functions and code here
 function destructivelyAppendKitten(name) {kittens.push(name);
 return kittens;
-}//send kieetend to the end of the array 
+}
+//send kieetend to the end of the array
 function Arrays destructivelyPrependKitten(name){kittens.unshift(name);
-return kittens;} //Arrays destructivelyPrependKitten(name)
+return kittens;}
+//Arrays destructivelyPrependKitten(name)
 // prepends a kitten to the beginning of the kittens array:
